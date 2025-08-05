c7731a7c7adcdf7a738f58fb0d47226c49a62b55
rngusrb <rngusrb1212@gmail.com>
2025-08-05 21:35:15 +0900

    str 오류 수정

--- routers/analysis.py
+++ routers/analysis.py
@@ -20,19 +20,19 @@ def floating_age(trdar_cd: int):
     return svc.plot_floating_gender_age_ratio(trdar_cd)
 
 @router.get("/sales/gender")
-def sales_gender(trdar_cd: int, induty_cd: int):
+def sales_gender(trdar_cd: int, induty_cd: str):
     return svc.plot_gender_sales_ratio(trdar_cd, induty_cd)
 
 @router.get("/sales/time")
-def sales_time(trdar_cd: int, induty_cd: int):
+def sales_time(trdar_cd: int, induty_cd: str):
     return svc.plot_time_sales_ratio(trdar_cd, induty_cd)
 
 @router.get("/sales/age")
-def sales_age(trdar_cd: int, induty_cd: int):
+def sales_age(trdar_cd: int, induty_cd: str):
     return svc.plot_age_sales_ratio(trdar_cd, induty_cd)
 
 @router.get("/sales/monthly")
-def sales_monthly(trdar_cd: int, induty_cd: int):
+def sales_monthly(trdar_cd: int, induty_cd: str):
     return svc.plot_monthly_sales_per_store(trdar_cd, induty_cd)
 
 @router.get("/sales/area-average")
@@ -40,19 +40,19 @@ def sales_area_avg(trdar_cd: int):
     return svc.plot_avg_sales_per_store_by_area(trdar_cd)
 
 @router.get("/store/count")
-def store_count(trdar_cd: int, induty_cd: int):
+def store_count(trdar_cd: int, induty_cd: str):
     return svc.plot_store_count_trend(trdar_cd, induty_cd)
 
 @router.get("/store/open")
-def store_open(trdar_cd: int, induty_cd: int):
+def store_open(trdar_cd: int, induty_cd: str):
     return svc.plot_open_store_trend(trdar_cd, induty_cd)
 
 @router.get("/store/close")
-def store_close(trdar_cd: int, induty_cd: int):
+def store_close(trdar_cd: int, induty_cd: str):
     return svc.plot_close_store_trend(trdar_cd, induty_cd)
 
 @router.get("/store/franchise")
-def store_franchise(trdar_cd: int, induty_cd: int):
+def store_franchise(trdar_cd: int, induty_cd: str):
     return svc.plot_franchise_ratio(trdar_cd, induty_cd)
 
 @router.get("/area/summary")
